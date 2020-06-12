6fae9e5336883db5711852271324136bbfc6574e
liuhe <he_liu@xtev.cn>
2020-06-12 16:08:35 +0800

    [add]使用eureka代替固定url

--- service-consumer/src/main/java/com/liuhedev/service/controller/UserController.java
+++ service-consumer/src/main/java/com/liuhedev/service/controller/UserController.java
@@ -3,8 +3,12 @@ package com.liuhedev.service.controller;
 
 import com.liuhedev.service.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author liuhe
@@ -17,9 +21,19 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient; // eureka客户端，可以获取到eureka中服务的信息
+
     @GetMapping
     public User queryUserById(@RequestParam("id") Long id) {
-        User user = this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
+        // 根据服务名称，获取服务实例。有可能是集群，所以是service实例集合
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+        // 因为只有一个Service-provider。所以获取第一个实例
+        ServiceInstance instance = instances.get(0);
+        // 获取ip和端口信息，拼接成服务地址
+        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
+        User user = this.restTemplate.getForObject(baseUrl, User.class);
+//        User user = this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
         return user;
     }
 }
