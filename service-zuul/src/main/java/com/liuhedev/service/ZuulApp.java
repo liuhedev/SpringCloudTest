35055eef4755e8cdce583a63508aebfbfa688b83
liuhe <he_liu@xtev.cn>
2020-06-15 14:55:46 +0800

    [fix]zull自动集成ribbon和hystrix，相应的默认配置导致特别容易熔断，所以在appliation.yml中配置修改。

--- service-zuul/src/main/java/com/liuhedev/service/ZuulApp.java
+++ service-zuul/src/main/java/com/liuhedev/service/ZuulApp.java
@@ -2,6 +2,7 @@ package com.liuhedev.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy // 开启网关功能
+@EnableDiscoveryClient
 public class ZuulApp {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApp.class, args);
