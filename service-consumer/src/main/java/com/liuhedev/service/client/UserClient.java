3dc5d7db34383571a5d6175f8b25f047a2fabb0f
liuhe <he_liu@xtev.cn>
2020-06-15 12:00:37 +0800

    [add]配置feign日志

--- service-consumer/src/main/java/com/liuhedev/service/client/UserClient.java
+++ service-consumer/src/main/java/com/liuhedev/service/client/UserClient.java
@@ -1,5 +1,6 @@
 package com.liuhedev.service.client;
 
+import com.liuhedev.service.config.FeignLogConfiguration;
 import com.liuhedev.service.pojo.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @author liuhe
  * @date 2020/06/15
  */
-@FeignClient(value = "service-provider") // 标注该类是一个feign接口
+@FeignClient(value = "service-provider", fallback = UserClientFallback.class, configuration = FeignLogConfiguration.class)
+// 标注该类是一个feign接口
 public interface UserClient {
 
     @GetMapping("user/{id}")
